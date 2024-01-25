04445213e3e619f35ba7a6ed046c4b3620094229
ivanshifman <ivanshifman1300@gmail.com>
2024-01-25 18:56:00 -0300

    preparando detalles comienzo segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
+
 
 function App() {
   return (
     <>
-      <NavBar />
-      <ItemDetailContainer />
-      <ItemListContainer />
+        <NavBar />
+        <ItemListContainer />
+        <ItemDetailContainer />
     </>
   );
 }
