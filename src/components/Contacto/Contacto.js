2ccd553f4c065e281a970471a07ea55f57dac488
ivanshifman <ivanshifman1300@gmail.com>
2024-01-28 15:42:55 -0300

    formulario terminado

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -1,4 +1,6 @@
 import './contacto.css';
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import EmailIcon from '@mui/icons-material/Email';
 import PhoneForwardedIcon from '@mui/icons-material/PhoneForwarded';
@@ -9,6 +11,34 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import HelpIcon from '@mui/icons-material/Help';
 
 const Contacto = () => {
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm()
+
+  const onSubmit = handleSubmit(async (data) => {
+    const shouldReset = await showConfirmation();
+    console.log(data)
+
+    if (shouldReset) {
+      reset();
+    }
+  });
+
+  const showConfirmation = async () => {
+    const result = await Swal.fire({
+      title: 'Muchas gracias!!!',
+      text: 'Sus datos han sido enviados',
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+      allowOutsideClick: false,
+      customClass: {
+        icon: 'iconoAlertaUno',
+      },
+    });
+
+    return result.isConfirmed;
+  };
+
+
   return (
     <div className='formulario'>
       <div className='contact-cont'>
@@ -18,34 +48,113 @@ const Contacto = () => {
         <div className='box'>
           <div className='contact form'>
             <h3>Envía tu mensaje</h3>
-            <form>
+            <form onSubmit={onSubmit}>
               <div className='formBox'>
                 <div className='row50'>
                   <div className='inputBox'>
                     <label htmlFor='nombre'>Nombre</label>
-                    <input type='text' placeholder='Nombre' id='nombre' name='nombre' autoComplete='given-name'/>
+                    <input type='text' placeholder='Nombre' id='nombre' {...register("nombre",
+                      {
+                        required: {
+                          value: true,
+                          message: "Nombre es requerido"
+                        },
+                        pattern: {
+                          value: /^[a-zA-Z]+$/,
+                          message: "Nombre no debe contener números ni espacios"
+                        },
+                        minLength: {
+                          value: 3,
+                          message: "Nombre debe tener al menos 3 caracteres"
+                        },
+                        maxLength: {
+                          value: 20,
+                          message: "Nombre debe ser menor a 20 caracteres"
+                        }
+                      })} />
+                    {errors.nombre && <span>{errors.nombre.message}</span>}
                   </div>
                   <div className='inputBox'>
                     <label htmlFor='apellido'>Apellido</label>
-                    <input type='text' placeholder='Apellido' id='apellido' name='apellido' autoComplete='family-name'/>
+                    <input type='text' placeholder='Apellido' id='apellido' {...register("apellido", {
+                      required: {
+                        value: true,
+                        message: "Apellido es requerido"
+                      },
+                      pattern: {
+                        value: /^[a-zA-Z]+$/,
+                        message: "Apellido no debe números ni espacios"
+                      },
+                      minLength: {
+                        value: 3,
+                        message: "Apellido debe tener al menos 3 caracteres"
+                      },
+                      maxLength: {
+                        value: 20,
+                        message: "Apellido debe ser menor a 20 caracteres"
+                      }
+                    })} />
+                    {errors.apellido && <span>{errors.apellido.message}</span>}
                   </div>
                 </div>
 
                 <div className='row50'>
                   <div className='inputBox'>
                     <label htmlFor='email'>Email</label>
-                    <input type='email' placeholder='Email' id='email' name='email' autoComplete='email'/>
+                    <input type='email' placeholder='Email' id='email' {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Correo es requerido"
+                      },
+                      pattern: {
+                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                        message: "Correo no válido"
+                      }
+                    })} />
+                    {errors.email && <span>{errors.email.message}</span>}
                   </div>
                   <div className='inputBox'>
                     <label htmlFor='numero'>Número</label>
-                    <input type='tel' placeholder='Numero' id='numero' name='numero' autoComplete='tel'/>
+                    <input type='tel' placeholder='Numero' id='numero' {...register("numero", {
+                      required: {
+                        value: true,
+                        message: "Número es requerido"
+                      },
+                      pattern: {
+                        value: /^[0-9]+$/,
+                        message: "Número debe contener solo números"
+                      },
+                      minLength: {
+                        value: 8,
+                        message: "Número debe tener al menos 8 caracteres"
+                      },
+                      maxLength: {
+                        value: 15,
+                        message: "Número debe ser menor a 15 caracteres"
+                      }
+                    })} />
+                    {errors.numero && <span>{errors.numero.message}</span>}
                   </div>
                 </div>
 
                 <div className='row100'>
                   <div className='inputBox'>
                     <label htmlFor='mensaje'>Mensaje</label>
-                    <textarea id='mensaje' placeholder='Escribe tu mensaje' name='mensaje' autoComplete='off' ></textarea>
+                    <textarea id='mensaje' placeholder='Escribe tu mensaje' {...register("mensaje", {
+                      required: {
+                        value: true,
+                        message: "Mensaje es requerido"
+                      },
+                      minLength: {
+                        value: 20,
+                        message: "El mensaje debe tener más de 20 caracteres"
+                      },
+                      maxLength: {
+                        value: 220,
+                        message: "El mensaje debe tener menos de 220 caracteres"
+                      }
+                    })}></textarea>
+                    {errors.mensaje && <span>{errors.mensaje.message}</span>}
                   </div>
                 </div>
 
