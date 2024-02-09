39782e2687feda97ece95352a620595530d8ac3e
ivanshifman <ivanshifman1300@gmail.com>
2024-02-09 16:27:27 -0300

    mas cambios css

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,34 +1,37 @@
+import { Link } from "react-router-dom";
 import ItemCart from "../ItemCart/ItemCart";
 import "./cart.css";
 
 const Cart = () => {
   return (
     <div className="contenedor-carrito">
-        <h2>Carrito</h2>
+      <h2>Carrito</h2>
       <div className="contenedor-cart">
         <div className="carrito">
-            <div className="producto-carrito">
-                <ItemCart />
-            </div>
+          <div className="producto-carrito">
+            <ItemCart />
+          </div>
         </div>
         <div className="aside-carrito">
-            <div className="min-aside">
-                <div className="p-items-carrito">
-                    <p>Cantidad de items:</p>
-                    <span>14</span>
-                </div>
+          <div className="min-aside">
+            <div className="p-items-carrito">
+              <p>Cantidad de items:</p>
+              <span>14</span>
             </div>
-            <div className="min-aside">
-                <div className="p-items-carrito">
-                    <p>Total</p>
-                    <span>$15000</span>
-                </div>
+          </div>
+          <div className="min-aside">
+            <div className="p-items-carrito">
+              <p>Total</p>
+              <span>$15000</span>
             </div>
-            <div className="botones">
-                <button className="comprar">Finalizar compra</button>
-                <button className="seguir-comprar">Seguir comprando</button>
-                <button className="eliminar-carrito">Vaciar carrito</button>
-            </div>
+          </div>
+          <div className="botones">
+            <button className="comprar">Finalizar compra</button>
+            <Link className="boton" to={`/`}>
+              <button className="seguir-comprar">Seguir comprando</button>
+            </Link>
+            <button className="eliminar-carrito">Vaciar carrito</button>
+          </div>
         </div>
       </div>
     </div>
