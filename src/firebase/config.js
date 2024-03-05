ca6085c2430b8254c65381ed4b8a42d69dfaca16
ivanshifman <ivanshifman1300@gmail.com>
2024-03-04 22:15:10 -0300

    oculto credencial

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPexwJYYaVQnUBDcfjyy_gH4FXGar3Dok",
-  authDomain: "shifman-react.firebaseapp.com",
-  projectId: "shifman-react",
-  storageBucket: "shifman-react.appspot.com",
-  messagingSenderId: "557497014874",
-  appId: "1:557497014874:web:aeb533a4b026a7ec089663",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
